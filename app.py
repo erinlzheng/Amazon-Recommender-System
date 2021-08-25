84f80da2dc9b5f8e48302c51d1cad5054df53e39
Erin Zheng <erinlzheng@gmail.com>
2021-08-24 21:03:33 -0400

    flask

--- app.py
+++ app.py
@@ -1,10 +1,58 @@
 from flask import Flask, render_template, jsonify
 from flask_cors import CORS
 import json
+import numpy as np 
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2 as pg2
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+import sklearn.externals
+import joblib
+import scipy.sparse
+from scipy.sparse import csr_matrix
+from scipy.sparse.linalg import svds
+import warnings; warnings.simplefilter('ignore')
+from sklearn.decomposition import TruncatedSVD
 
 app = Flask(__name__)
-CORS(app, support_credentials = True)
+CORS(app, support_credentials=True)
 
+# DEF
+def get_recommendations(asin):
+    new_df = merged_df.head(5000)
+    ratings_matrix = new_df.pivot_table(values='rating', index='user', columns='asin', fill_value=0)
+
+    X = ratings_matrix.T
+    X1 = X
+
+
+    SVD = TruncatedSVD(n_components=10)
+    decomposed_matrix = SVD.fit_transform(X)
+    decomposed_matrix.shape
+    correlation_matrix = np.corrcoef(decomposed_matrix)
+    correlation_matrix.shape
+    i = "B00004TDA2"
+    product_id = list(X.index)
+    asin_id = product_id.index(i)
+    asin_id
+    correlation_asin_id = correlation_matrix[asin_id]
+    correlation_asin_id.shape
+    Recommend = list(X.index[correlation_asin_id > 0.65])
+
+    # Removes the purchased item 
+    Recommend.remove(i) 
+    Recommend[0:10]
+    products = Recommend[0:10]
+    for i in products:
+        url = 'https://www.amazon.com/s?k='+i+'&ref=nb_sb_noss'
+        print (url)
+
+
+# APP ROUTES
 @app.route("/index")
 @app.route("/index.html")
 @app.route("/")
@@ -17,12 +65,15 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route("/products")
-@app.route("/products.html")
-def change():
-    return render_template('produts.html')
 
-
+@app.route("/products.html", methods=['GET', 'POST'])
+@app.route("/products", methods=['GET', 'POST'])
+def products():
+    return render_template('products.html')
+def main():
+    if flask.request.method == ‘GET’:
+        return(flask.render_template(‘index.html’))
+        
 @app.route("/insights")
 @app.route("/insights.html")
 def insights():
