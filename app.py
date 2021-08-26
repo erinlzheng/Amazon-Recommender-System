23da4eb9fc01dfd4a49e1547211d4f7725d6bf9e
JohnTimmers <john.timmers16@gmail.com>
2021-08-26 17:33:58 -0400

    config vars p3

--- app.py
+++ app.py
@@ -21,11 +21,12 @@ from scipy.sparse.linalg import svds
 import warnings; warnings.simplefilter('ignore')
 from sklearn.decomposition import TruncatedSVD
 from boto.s3.connection import S3Connection
+import os
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
-s3 = S3Connection(os.environ['postgres'], os.environ['postgres'])
-s3_handler = S3Handler(os.environ.get('postgres'), os.environ.get('postgres'))
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+s3_handler = S3Handler(os.environ.get('S3_KEY'), os.environ.get('S3_SECRET'))
 
 data = pd.read_csv('C:\\Users\\erinz\\Downloads\\ez boot camp\\Projects\\Project-3-Recommender-System\\static\\data\\merged.csv') 
 df = data[['asin', 'user', 'rating']] 
